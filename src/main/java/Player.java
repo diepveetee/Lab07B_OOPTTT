3e0b1b21ff0c26b032b53172541581e7b7becd83
Dannik <diepvt@mail.uc.edu>
2026-03-23 23:29:25 -0400

    Added Player

---
+++ src/main/java/Player.java
@@ -0,0 +1,28 @@
+public class Player {
+
+    private final String name;
+    private final char symbol;
+    private int wins;
+
+    public Player(String name, char symbol) {
+        this.name = name;
+        this.symbol = symbol;
+        this.wins = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void incrementWins() {
+        wins++;
+    }
+}
